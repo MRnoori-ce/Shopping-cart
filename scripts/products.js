20e9eb46e17b04f8ec9fa2a5f37d658ed4611270
MRnoori-ce <125483337+MRnoori-ce@users.noreply.github.com>
2023-07-12 23:00:30 +0330

    First commit of project

---
+++ scripts/products.js
@@ -0,0 +1,60 @@
+let products= [
+    {
+        id: 0,
+        name:'آیفون ۱۲',
+        price: 50000000,
+        instock: 4,
+        imgSrc:'../images/iphone.png'
+    },
+    {
+        id: 1,
+        name:' کفش نایک ',
+        price: 800000,
+        instock: 8,
+        imgSrc:'../images/shoes.png'
+    },
+    {
+        id: 2,
+        name:' لپ تاپ ایسر',
+        price: 33000000,
+        instock: 3,
+        imgSrc:'../images/laptop.png'
+    },
+    {
+        id: 3,
+        name:' کوله پشتی',
+        price: 500000,
+        instock: 12,
+        imgSrc:'../images/backpack.png'
+    },
+    {
+        id: 4,
+        name:' ماگ قرمز',
+        price: 40000,
+        instock:18,
+        imgSrc:'../images/mug.png'
+    },
+    {
+        id: 5,
+        name:' تی‌شرت مردانه خاکستری',
+        price: 100000,
+        instock: 20,
+        imgSrc:'../images/tshirt.png'
+    },
+    {
+        id: 6,
+        name:' ساعت اسپرت',
+        price: 2500000,
+        instock: 6,
+        imgSrc:'../images/watch.png'
+    },
+    {
+        id: 7,
+        name:' هدفون',
+        price: 700000,
+        instock: 9,
+        imgSrc:'../images/headphone.png'
+    },
+]
+
+alert('hi');
